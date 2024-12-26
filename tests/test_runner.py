eab341131dd48d8119d49e0d252a96209ab0b33c
andriyav <aandrusy@softserveinc.com>
2024-12-26 02:13:10 +0200

    run ci/dc

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -18,6 +18,32 @@ class BaseTestRunner(unittest.TestCase):
     def setUp(self):
         self._init_driver()
         self._login()
+    #
+    # def _init_driver(self):
+    #     chrome_options = webdriver.ChromeOptions()
+    #     chrome_options.add_argument('--headless')
+    #     chrome_options.add_argument('--disable-gpu')
+    #     chrome_options.add_argument("--no-sandbox")
+    #     chrome_options.add_argument("--disable-dev-shm-usage")
+    #     chrome_options.add_argument("--disable-popup-blocking")
+    #     chrome_options.add_argument('--disable-web-security')
+    #     chrome_options.add_argument('--disable-extensions')
+    #     chrome_options.add_argument('--window-size=1920x1080')
+    #     chrome_options.add_argument(f"user-data-dir={CHROME_USER_DIR}")
+    #     chrome_options.add_argument("profile-directory=Default")
+    #     self.driver = webdriver.Chrome(options=chrome_options)
+    #     self.driver.implicitly_wait(IMPLICITLY_WAIT)
+    #     self.driver.get(ValueProvider.get_base_url())
+    #
+    # def _login(self):
+    #     self.driver.implicitly_wait(10)
+    #     LoginComponent(self.driver).click_authorisation_btn()
+    #     self.driver.maximize_window()
+    #
+    # def tearDown(self):
+    #     self.driver.quit()
+
+    '''Login with username and password'''
 
     def _init_driver(self):
         chrome_options = webdriver.ChromeOptions()
@@ -29,44 +55,18 @@ class BaseTestRunner(unittest.TestCase):
         chrome_options.add_argument('--disable-web-security')
         chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--window-size=1920x1080')
-        chrome_options.add_argument(f"user-data-dir={CHROME_USER_DIR}")
-        chrome_options.add_argument("profile-directory=Default")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
+        self.driver.maximize_window()
         self.driver.get(ValueProvider.get_base_url())
 
     def _login(self):
         self.driver.implicitly_wait(10)
         LoginComponent(self.driver).click_authorisation_btn()
+        LoginModal(self.driver).set_email(ValueProvider.get_email())
+        LoginModal(self.driver).click_next_button_first()
+        LoginModal(self.driver).set_password(ValueProvider.get_password())
+        LoginModal(self.driver).click_next_button_second()
         self.driver.maximize_window()
-
-    def tearDown(self):
-        self.driver.quit()
-
-    '''Login with username and password'''
-
-    # def _init_driver(self):
-    #     chrome_options = webdriver.ChromeOptions()
-    #     chrome_options.add_argument('--headless')
-    #     chrome_options.add_argument('--disable-gpu')
-    #     chrome_options.add_argument("--no-sandbox")
-    #     chrome_options.add_argument("--disable-dev-shm-usage")
-    #     chrome_options.add_argument("--disable-popup-blocking")
-    #     chrome_options.add_argument('--disable-web-security')
-    #     chrome_options.add_argument('--disable-extensions')
-    #     chrome_options.add_argument('--window-size=1920x1080')
-    #     self.driver = webdriver.Chrome(options=chrome_options)
-    #     self.driver.implicitly_wait(IMPLICITLY_WAIT)
-    #     self.driver.maximize_window()
-    #     self.driver.get(ValueProvider.get_base_url())
-    #
-    # def _login(self):
-    #     self.driver.implicitly_wait(10)
-    #     LoginComponent(self.driver).click_authorisation_btn()
-    #     LoginModal(self.driver).set_email(ValueProvider.get_email())
-    #     LoginModal(self.driver).click_next_button_first()
-    #     LoginModal(self.driver).set_password(ValueProvider.get_password())
-    #     LoginModal(self.driver).click_next_button_second()
-    #     self.driver.maximize_window()
-    #     time.sleep(5)
+        time.sleep(5)
 
