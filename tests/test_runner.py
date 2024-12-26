bd9dc70416e4353a64277f850ab78b75f2a0ac89
andriyav <aandrusy@softserveinc.com>
2024-12-26 02:25:54 +0200

    run ci/dc

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -47,7 +47,6 @@ class BaseTestRunner(unittest.TestCase):
 
     def _init_driver(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
