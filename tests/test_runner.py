1c2e88cb546cfee1e2a2e6b895dd0b4e811a7864
andriyav <aandrusy@softserveinc.com>
2024-12-27 09:49:04 +0200

    ci/cd

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -62,6 +62,8 @@ class BaseTestRunner(unittest.TestCase):
         LoginModal(self.driver).click_next_button_second()
         self.driver.maximize_window()
         time.sleep(5)
+        with open("page_source.html", "w", encoding="utf-8") as f:
+            f.write(self.driver.page_source)
 
     def tearDown(self):
         self.driver.quit()
