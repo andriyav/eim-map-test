ac949b3fd913d929544cef6caa3120e6f429508b
andriyav <aandrusy@softserveinc.com>
2024-12-28 10:33:59 +0200

    google

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -7,8 +7,8 @@ from SLP.ui.PageObjects.SLPlogin.slp_login import LoginComponent
 from SLP.ui.PageObjects.login_modal.login_modal import LoginModal
 from data.value_provider import ValueProvider
 
-CHROME_USER_DIR = '/home/runner/.config/google-chrome/'
-# CHROME_USER_DIR = os.path.abspath("./SLPUI/data/cash2")
+# CHROME_USER_DIR = '/home/runner/.config/google-chrome/'
+CHROME_USER_DIR = os.path.abspath("~/.cache/google-chrome/")
 
 IMPLICITLY_WAIT = 10
 
