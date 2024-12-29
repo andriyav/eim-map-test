956ea1511a7e8b279b075e5df59b097ceaf4d956
andriyav <aandrusy@softserveinc.com>
2024-12-29 22:50:50 +0200

    kw_id = "741","316", "520"

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -7,7 +7,6 @@ from SLP.ui.PageObjects.login_modal.login_modal import LoginModal
 from data.value_provider import ValueProvider
 
 CHROME_USER_DIR = '/home/runner/.config/google-chrome/'
-# CHROME_USER_DIR = os.path.abspath("./SLPUI/data/cash2")
 
 IMPLICITLY_WAIT = 10
 
@@ -20,27 +19,6 @@ class BaseTestRunner(unittest.TestCase):
     def setUp(self):
         self._init_driver()
         self._login()
-
-    # def _init_driver(self):
-    #     chrome_options = webdriver.ChromeOptions()
-    #     chrome_options.add_argument('--disable-gpu')
-    #     chrome_options.add_argument('--no-sandbox')
-    #     chrome_options.add_argument('--disable-dev-shm-usage')
-    #     chrome_options.add_argument('--disable-popup-blocking')
-    #     chrome_options.add_argument('--window-size=1920x1080')
-    #     chrome_options.add_argument(f"user-data-dir={CHROME_USER_DIR}")
-    #     chrome_options.add_argument("profile-directory=Default")
-    #     self.driver = webdriver.Chrome(options=chrome_options)
-    #     self.driver.implicitly_wait(IMPLICITLY_WAIT)
-    #     self.driver.get(ValueProvider.get_base_url())
-    #     print(f"Resolved CHROME_USER_DIR path: {os.path.abspath(CHROME_USER_DIR)}")
-    #
-    # def _login(self):
-    #     self.driver.implicitly_wait(10)
-    #     LoginComponent(self.driver).click_authorisation_btn()
-    #     self.driver.maximize_window()
-
-
 
     '''Login with username and password'''
 
@@ -56,14 +34,11 @@ class BaseTestRunner(unittest.TestCase):
     def _login(self):
         self.driver.implicitly_wait(10)
         LoginComponent(self.driver).click_authorisation_btn()
-        LoginModal(self.driver).set_email(ValueProvider.get_email())
-        LoginModal(self.driver).click_next_button_first()
-        LoginModal(self.driver).set_password(ValueProvider.get_password())
-        LoginModal(self.driver).click_next_button_second()
+        # LoginModal(self.driver).set_email(ValueProvider.get_email())
+        # LoginModal(self.driver).click_next_button_first()
+        # LoginModal(self.driver).set_password(ValueProvider.get_password())
+        # LoginModal(self.driver).click_next_button_second()
         self.driver.maximize_window()
-        time.sleep(5)
-        with open("page_source.html", "w", encoding="utf-8") as f:
-            f.write(self.driver.page_source)
 
     def tearDown(self):
         self.driver.quit()
