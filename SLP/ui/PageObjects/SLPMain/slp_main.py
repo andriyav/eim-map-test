4413c8cdfef0aab70f4ac845af3ef423e588b1f9
aandrusyk <139917933+aandrusyk@users.noreply.github.com>
2025-02-15 22:28:49 +0200

    media: test_list_address_properties_country

--- SLP/ui/PageObjects/SLPMain/slp_main.py
+++ SLP/ui/PageObjects/SLPMain/slp_main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from SLP.ui.Elements.button import Button
 from SLP.ui.PageObjects.SLPMain.listing_component import ListComponent
+from SLP.ui.PageObjects.SLPMain.madia_component import MediaComponent
 from SLP.ui.PageObjects.SLPMain.source_select_component import SourceSelectComponent
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -14,6 +15,7 @@ PHOTO_TAB = (By.XPATH, '/html/body/section/div/div/div/div[1]/div/div/a[2]')
 SAVE_MAP_BTN = (By.CSS_SELECTOR, '#btn_save_map')
 DATE_SAVE = (By.ID, 'last_edited')
 IMPLIS_WAIT_MAP = (By.CSS_SELECTOR, '#listing_mapper_list_category__0')
+IMPLIS_WAIT_MEDIA_MAP = (By.CSS_SELECTOR, '#photo_mapper_mls_number__0')
 SOURCE_SELECT = (By.CSS_SELECTOR, '#sources')
 SPINNER = (By.CSS_SELECTOR, '#spin_save_map')
 MLS_BTN = (By.CSS_SELECTOR, '#navbarNav > ul.navbar-nav.me-auto.mb-2.mb-lg-0 > li:nth-child(2) > a')
@@ -59,6 +61,10 @@ class SLPMain(BasePage):
     @allure.step("Implicit wait metadata")
     def impl_wait_metadata(self):
         ListComponent(self.driver).get_map_filed(IMPLIS_WAIT_MAP)
+
+    @allure.step("Implicit wait metadata")
+    def impl_wait_media_metadata(self):
+        MediaComponent(self.driver).get_map_filed(IMPLIS_WAIT_MEDIA_MAP)
 
     @allure.step("Wait until spinner available")
     def wait_until_spinner_available(self):
